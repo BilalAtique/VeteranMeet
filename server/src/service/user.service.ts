9829dcb3d49287ab7ebbaeadea8542032c5ff400
Bilal Atique <74999164+MuhammadBilalBinAtique@users.noreply.github.com>
2022-05-21 15:04:08 +0500

    implemented login

---
+++ server/src/service/user.service.ts
@@ -0,0 +1,11 @@
+import userModel from "../model/user.model";
+
+const createUser = async (input: any) => {
+  try {
+    return await userModel.create(input);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default createUser;
