6c25587f5833d580774cddf501f03c2466301c65
Taha Ahmed Rasheed <tahaahmedrasheedpk@gmail.com>
2022-05-21 16:22:55 +0500

    Alwidaii commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,67 @@
-// import React from 'react';
+import { useState } from "react";
 import "./App.css";
-import { allEntitySectors } from "@bitflipz/shared/build/Entity";
+import { allEntitySectors, EntitySector } from "@bitflipz/shared/build/Entity";
 import { capitalizeFirstLetter } from "@bitflipz/shared/build/util";
 
 function App() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [location, setLocation] = useState("");
+  const [sectorSet, setSectorSet] = useState<Set<EntitySector>>(new Set([]));
+  /* const [name, setName] = useState(""); */
+
   return (
     <form>
       <label>
         Name:
-        <input type="text" />
+        <input
+          type="text"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </label>
       <br />
       <label>
         Email:
-        <input type="email" />
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
       </label>
       <br />
       <label>
         Password:
-        <input type="password" />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
       </label>
       <br />
       <label>
         Location:
-        <input type="text" />
+        <input
+          type="text"
+          value={location}
+          onChange={(event) => setLocation(event.target.value)}
+        />
       </label>
       <br />
+      <div>
+        <div className="sectorList">
+          {Array.from(sectorSet).map((sector) => (
+            <>
+              <div>
+                {sector}{" "}
+            <button onClick={() => setSectorSet(new Set([]))}>Remove</button>{" "}
+                  </div>
+              <br />
+            </>
+          ))}
+        </div>
+      </div>
       <label>
         Profession & hobbies:
         <select>
